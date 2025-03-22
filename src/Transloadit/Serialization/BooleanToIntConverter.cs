831b0ebece6bd605bc030f04829c0eb487aaaaa8
Oleksandr Omelchenko <s.tet@meta.ua>
2025-03-22 14:58:14 +0100

    Use bool

---
+++ src/Transloadit/Serialization/BooleanToIntConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Transloadit.Serialization
+{
+    /// <summary>
+    /// Represents <c>int</c> to <c>bool</c> converter used to convert <c>0</c> to <c>false</c> and other values to <c>true</c>.
+    /// </summary>
+    public class BooleanToIntConverter : JsonConverter
+    {
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue((bool)value ? 1 : 0);
+        }
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            int integer = Convert.ToInt32(reader.Value);
+            return integer != 0;
+        }
+
+        /// <inheritdoc />
+        public override bool CanConvert(Type objectType) => objectType == typeof(bool);
+    }
+}
