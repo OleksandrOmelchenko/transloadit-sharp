e6d00c8164c87e1ca3b7ca65383e573684bd34a3
Oleksandr Omelchenko <s.tet@meta.ua>
2025-03-26 21:54:11 +0100

    Added smart cdn service

---
+++ src/Transloadit/Polyfills/DateTimeOffsetExtensions.cs
@@ -0,0 +1,15 @@
+﻿#if NET452
+using System;
+
+namespace Transloadit.Polyfills
+{
+    internal static class DateTimeOffsetExtensions
+    {
+        public static long ToUnixTimeMilliseconds(this DateTimeOffset dateTimeOffset)
+        {
+            var unixEpoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+            return (long)(dateTimeOffset - unixEpoch).TotalMilliseconds;
+        }
+    }
+}
+#endif
---
+++ src/Transloadit/Services/SignatureService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using Transloadit.Utilities;
+
+namespace Transloadit.Services
+{
+    /// <summary>
+    /// Represents tools for working with signatures.
+    /// </summary>
+    public class SignatureService
+    {
+        private readonly string _secret;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignatureService"/> class with Transloadit secret.
+        /// </summary>
+        /// <param name="secret">Transloadit auth secret.</param>
+        public SignatureService(string secret)
+        {
+            _secret = secret;
+        }
+
+        /// <summary>
+        /// Calculates a signature for the specified string.
+        /// </summary>
+        /// <param name="input">The string to calculate signature for.</param>
+        /// <param name="algorithm">The hashing algorithm to use.</param>
+        /// <returns>The signature as <c>algorithm:hash</c>.</returns>
+        /// <exception cref="InvalidEnumArgumentException"></exception>
+        public string CalculateSignature(string input, SignatureAlgorithm algorithm = SignatureAlgorithm.Sha384)
+            => SignatureUtilities.CalculateSignature(input, _secret, algorithm);
+
+        /// <summary>
+        /// Validates the signature against the signature of the provided string.
+        /// </summary>
+        /// <param name="input">The string to calculate signature for.</param>
+        /// <param name="signature">The signature to validate against.</param>
+        /// <returns>A value indicating whether given <paramref name="signature"/> matches with the signature of <paramref name="input"/>.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public bool ValidateSignature(string input, string signature)
+            => SignatureUtilities.ValidateSignature(input, _secret, signature);
+    }
+}
